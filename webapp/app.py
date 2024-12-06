7deada152273248ab4d528f868aa25c34e7fec10
immortalcodes <21112002mj@gmail.com>
2024-12-06 15:57:07 +0530

    created error templates

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,9 @@
 from canonicalwebteam.flask_base.app import FlaskBase
 from canonicalwebteam.templatefinder import TemplateFinder
+from canonicalwebteam import image_template
+
+from flask import render_template
+
 
 app = FlaskBase(
     __name__,
@@ -7,6 +11,19 @@ app = FlaskBase(
     template_folder="../templates",
     static_folder="../static",
 )
+
+
+@app.context_processor
+def utility_processor():
+    return {"image": image_template}
+
+@app.errorhandler(Exception)
+def render_error_page(error):
+    error_code = getattr(error, "code", 500)
+    error_message = getattr(error, "description", "Something went wrong!")
+    return render_template(
+        "error.html", error_code=int(error_code), error_message=error_message
+    )
 
 template_finder_view = TemplateFinder.as_view("template_finder")
 app.add_url_rule("/", view_func=template_finder_view)
