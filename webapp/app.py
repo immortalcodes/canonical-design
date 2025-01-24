fddd4ed365bbc9fc35a210ba6df5c2a3ab6f9332
immortalcodes <21112002mj@gmail.com>
2025-01-24 19:19:16 +0530

    review changes

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,7 @@
 import requests
 
 from flask import render_template, request
-from urllib.parse import parse_qs, urlencode, urlparse
+from urllib.parse import parse_qs, urlencode
 
 from canonicalwebteam.flask_base.app import FlaskBase
 from canonicalwebteam.blog import build_blueprint, BlogViews, BlogAPI
@@ -18,13 +18,13 @@ app = FlaskBase(
 session = requests.Session()
 
 
-# @app.errorhandler(Exception)
-# def render_error_page(error):
-#     error_code = getattr(error, "code", 500)
-#     error_message = getattr(error, "description", "Something went wrong!")
-#     return render_template(
-#         "error.html", error_code=int(error_code), error_message=error_message
-#     )
+@app.errorhandler(Exception)
+def render_error_page(error):
+    error_code = getattr(error, "code", 500)
+    error_message = getattr(error, "description", "Something went wrong!")
+    return render_template(
+        "error.html", error_code=int(error_code), error_message=error_message
+    )
 
 
 blog_views = BlogViews(
@@ -54,27 +54,9 @@ def modify_query(params):
     return urlencode(query_params, doseq=True)
 
 
-def get_blogs():
-    blog_views = BlogViews(
-        api=BlogAPI(
-            session=session,
-            api_url="https://ubuntu.com/blog/wp-json/wp/v2",
-            thumbnail_width=354,
-            thumbnail_height=180,
-        ),
-        blog_title="Design blog",
-        tag_ids=[1239],
-        per_page=3,
-    )
-    blogs = blog_views.get_index()["articles"]
-    sorted_articles = sorted(blogs, key=lambda x: x["date"], reverse=True)
-    return sorted_articles
-
-
 @app.context_processor
 def utility_processor():
     return {
         "modify_query": modify_query,
-        "get_blogs": get_blogs,
         "image": image_template,
     }
